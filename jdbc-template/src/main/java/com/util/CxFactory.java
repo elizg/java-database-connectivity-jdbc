f6fa63959207ad615a8bcdd3fbe16094dc732fa3
Elizabeth Grauvogel <elizabethgrauvogel@gmail.com>
2020-07-24 21:02:11 -0700

    added connection factory

---
+++ jdbc-template/src/main/java/com/util/CxFactory.java
@@ -0,0 +1,62 @@
+package com.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class CxFactory {
+	
+	private static String url;
+	private static String user;
+	private static String password;
+	private static CxFactory cf;
+	private static final String PROPERTIES_FILE = "src/main/resources/database.properties";
+	
+	public static Connection getConnection() {
+		
+		if (cf == null) {
+			cf = new CxFactory();
+		}
+		
+		return cf.createConnection();
+		
+	}
+	
+	private CxFactory() {
+		
+		Properties prop = new Properties();
+		
+		try (FileInputStream fis = new FileInputStream(PROPERTIES_FILE)){
+			
+			prop.load(fis);
+			url = prop.getProperty("url");
+			user = prop.getProperty("user");
+			password = prop.getProperty("password");
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private Connection createConnection() {
+		
+		Connection conn = null;
+		
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+		
+	}
+
+}
